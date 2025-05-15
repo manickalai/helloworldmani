d64b017bed8d59d384840870e147680266d5435d
manickalai <manickalai@users.noreply.github.com>
2025-05-15 10:51:24 +0530

    Update Main.java
    3rd commend

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("my first java program");
-         System.out.printf("i have completed the Github demo");
+      //  System.out.printf("my first java program");
+        // System.out.printf("i have completed the Github demo");
+System.out.printf("welcome to my Java program");
 
 
     }
